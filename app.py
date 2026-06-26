2e99803f2a48b4a405c0a1c9729e5912cb5ec443
krish-49 <krishrathod5696@gmail.com>
2026-06-26 14:21:35 +0530

    Chat history

--- app.py
+++ app.py
@@ -3,8 +3,14 @@ import streamlit as st
 from dotenv import load_dotenv  
 from PyPDF2 import PdfReader
 from langchain_text_splitters import CharacterTextSplitter
-from langchain.embeddings import OpenAIEmbeddings
+# from langchain_openai import OpenAIEmbeddings
+from langchain_google_genai import GoogleGenerativeAIEmbeddings
 from langchain_community.vectorstores import FAISS
+from langchain_classic.memory import ConversationBufferMemory
+from langchain_classic.chains import ConversationalRetrievalChain
+# from langchain_openai import ChatOpenAI
+from langchain_google_genai import ChatGoogleGenerativeAI
+from htmlTemplates import css, bot_template, user_template
 
 def get_pdf_text(pdf_docs):   # get raw text from pdfs
     text = "" 
@@ -25,19 +31,62 @@ def get_text_chunks(text):   # splitting text into smaller chunks
     chunks = text_splitter.split_text(text)
     return chunks
 
-def getvectorstore(text_chunks):   # create vector store from text chunks
-    embeddings = OpenAIEmbeddings()
+def get_vectorstore(text_chunks):   # create vector store from text chunks
+    # embeddings = OpenAIEmbeddings()
+    embeddings = GoogleGenerativeAIEmbeddings(
+    model="models/gemini-embedding-2"
+)
     vectorstore = FAISS.from_texts(texts=text_chunks, embedding=embeddings)
     return vectorstore
+
+def get_conversation_chain(vectorstore):   # create a conversation chain
+    # llm = ChatOpenAI()
+    llm = ChatGoogleGenerativeAI(
+    model="gemini-2.5-flash",
+    temperature=0
+)
+    memory = ConversationBufferMemory(memory_key="chat_history", return_messages=True)
+    conversation_chain = ConversationalRetrievalChain.from_llm(
+        llm=llm,
+        retriever=vectorstore.as_retriever(),
+        memory=memory
+    )
+    return conversation_chain
+
+def handle_userQue(user_que):
+    response = st.session_state.conversation({'question' : user_que})
+    st.session_state.chat_history = response['chat_history']
+
+    for i, message in enumerate(st.session_state.chat_history):
+        if i % 2 == 0:
+            st.write(user_template.replace("{{MSG}}", message.content), unsafe_allow_html=True)
+
+        else:
+            st.write(bot_template.replace("{{MSG}}", message.content), unsafe_allow_html=True)
+
 
 def main():
     load_dotenv()  # Load env var from .env
 
     st.set_page_config(page_title="Ask multiple PDFs", page_icon=":books:", layout="wide")
 
+    st.write(css, unsafe_allow_html=True)
+
+    if "conversation" not in st.session_state:
+        st.session_state.conversation = None
+    
+    if "chat_history" not in st.session_state:
+        st.session_state.chat_history = None
+
     st.header("Ask multiple PDFs :books:")
 
-    st.text_input("Ask your question here:")
+    user_que = st.text_input("Ask your question here:")
+
+    if user_que:
+        handle_userQue(user_que)
+
+    st.write(user_template.replace("{{MSG}}", "Hello robot"), unsafe_allow_html=True)
+    st.write(bot_template.replace("{{MSG}}", "Hello dear"), unsafe_allow_html=True)
 
     with st.sidebar:
         st.subheader("Upload your PDFs / Documents")
@@ -58,6 +107,9 @@ def main():
                 # get vector store
                 vectorstore = get_vectorstore(text_chunks)
 
-
+                # create a conversation chain(a system)
+                st.session_state.conversation = get_conversation_chain(vectorstore)
+    
+    
 if __name__ == "__main__":
     main()
